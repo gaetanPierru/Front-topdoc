87aef418824381c0a4d97701fc4f278f0a2d5d8f
gaetanpierru <pierrugaetan0@gmail.com>
2023-01-11 16:58:32 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head> <title>Topdoc</title><link rel='icon' type='image/gif' href='leveinard.png'/></Head>
+      <Component {...pageProps} />
+  </>
 }
